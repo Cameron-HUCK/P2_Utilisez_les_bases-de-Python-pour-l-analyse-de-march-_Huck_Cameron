0024ea9992d6f30ca67a4e9c1e82d81cffaf1751
Cameron HUCK <huck-cameron@hotmail.fr>
2023-03-24 18:34:10 +0100

    Création du git et analyse des données d'une page produit

---
+++ product.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.contents)
