855d96e80cb28a05bf8f370f0c4f1d5881ed5644
Cameron HUCK <huck-cameron@hotmail.fr>
2023-03-24 19:14:28 +0100

    Etape 2 terminé

--- product.py
+++ product.py
@@ -1,6 +1,12 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.contents)
+
+data = {"Titre":[soup]}
+
+
+pd.DataFrame(data).to_csv("data/one_product.csv", sep=",", index=False)
+
