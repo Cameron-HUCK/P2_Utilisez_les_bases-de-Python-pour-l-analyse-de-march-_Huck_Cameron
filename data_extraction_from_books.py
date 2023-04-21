1a6335f02523e7ceba7d04813eed556260eb7b20
Cameron HUCK <huck-cameron@hotmail.fr>
2023-04-21 18:29:28 +0200

    Finalisation

--- data_extraction_from_books.py
+++ data_extraction_from_books.py
@@ -2,104 +2,69 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# URL de la page du livre "A Light in the Attic"
-book_url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+# Define the root URL of the site
+url = "http://books.toscrape.com/"
 
-# Obtenir le contenu HTML de la page du livre
-page = requests.get(book_url)
-soup = BeautifulSoup(page.content, "html.parser")
+# Define the function to extract the content from a webpage
+def extract(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    return soup
 
-# Extraire les informations du livre
-title = soup.find("h1").get_text()
-price = soup.find("p", class_="price_color").get_text()
-availability = soup.find("p", class_="availability").get_text().strip()
+# Define the function to transform the extracted data into a Pandas DataFrame
+def transform(data, category=None):
+    books = []
+    for item in data:
+        title = item.h3.a.get('title') if item.h3 and item.h3.a else ""
+        price = item.select('.price_color')[0].get_text()
+        availability = item.select('.availability')[0].get_text().strip()
+        img_url = url + item.img['src'].replace('../', '')
+        img_data = requests.get(img_url).content
+        img_name = item.img['src'].split("/")[-1]
+        img_path = f"data/images/{img_name}"
+        with open(img_path, 'wb') as handler:
+            handler.write(img_data)
+        books.append({'Title': title, 'Price': price, 'Availability': availability, 'Category': category})
+    return pd.DataFrame(books)
 
-# Créer un DataFrame pandas avec les informations extraites
-book_data = pd.DataFrame({
-    "Title": [title],
-    "Price": [price],
-    "Availability": [availability],
-}).to_csv("data/one_product.csv", sep=",", index=False)
-######################################################################################
 
-# URL de la page de la catégorie "MYSTERY"
-category_url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
+# Define the function to load the transformed data into a CSV file
+def load(data, filename):
+    data.to_csv(filename, sep=',', index=False)
 
-# Liste pour stocker les informations de tous les livres
+# Extract data for a single book
+soup = extract("http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
+data = transform([soup])
+load(data, "data/one_product.csv")
+
+# Extract data for all books in the Mystery category
+soup_category = extract('http://books.toscrape.com/catalogue/category/books/mystery_3/index.html')
 category_books = []
-# Boucle pour traiter toutes les pages de la catégorie "Mystery"
-while category_url:
-    category_page = requests.get(category_url)
-    category_soup = BeautifulSoup(category_page.content, 'html.parser')
-
-    # Boucle à travers tous les livres de la page
-    for article in category_soup.find_all('article', {'class': 'product_pod'}):
-        # Récupération des informations du livre
-        category_title = article.h3.a.get('title')
-        category_price = article.select('.price_color')[0].get_text()
-        category_availability = article.select('.availability')[0].get_text().strip()
-
-        # Ajout des informations du livre à la liste
-        category_books.append({'Titre': category_title, 'Prix': category_price, 'Disponibilité': category_availability})
-
-    # Récupération de l'URL de la page suivante
-    next_page = category_soup.find('li', {'class': 'next'})
+while soup_category:
+    data = soup_category.find_all('article', {'class': 'product_pod'})
+    category_books += transform(data, 'Mystery').to_dict('records')
+    next_page = soup_category.find('li', {'class': 'next'})
     if next_page:
         category_url = next_page.a.get('href')
-        # Modification de l'URL pour qu'elle soit absolue si elle est relative
         category_url = category_url if 'http' in category_url else f'http://books.toscrape.com/catalogue/category/books/mystery_3/{category_url}'
+        soup_category = extract(category_url)
     else:
-        category_url = None
+        soup_category = None
+load(pd.DataFrame(category_books), "data/all_book_one_category.csv")
 
-# Créer un DataFrame pandas avec les informations extraites et l'enregistrer dans un fichier CSV
-category_book_data = pd.DataFrame(category_books).to_csv("data/all_book_one_category.csv", sep=",", index=False)
-
-########################################################################################################
-# URL racine du site
-url = "http://books.toscrape.com/"
-# Récupération de la page d'accueil du site
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-# Récupération de toutes les catégories de livres
-all_book_category_links = soup.select('.side_categories > ul > li > ul > li > a')
-
-# Liste pour stocker les informations de tous les livres
+# Extract data for all books in all categories
+all_book_category_links = extract(url).select('.side_categories > ul > li > ul > li > a')
 all_books = []
-# Boucle pour traiter toutes les catégories de livres
-for all_book_category_link in all_book_category_links:
-    all_book_category_url = url + all_book_category_link.get('href')
-    # Boucle pour traiter toutes les pages de la catégorie
-    while all_book_category_url:
-        all_book_category_page = requests.get(all_book_category_url)
-        all_book_category_soup = BeautifulSoup(all_book_category_page.content, 'html.parser')
-
-        # Boucle à travers tous les livres de la page
-        for i, article in enumerate(all_book_category_soup.find_all('article', {'class': 'product_pod'})):
-            # Récupération des informations du livre
-            all_book_title = article.h3.a.get('title')
-            all_book_price = article.select('.price_color')[0].get_text()
-            all_book_availability = article.select('.availability')[0].get_text().strip()
-
-            img_url = url + article.img['src'].replace('../', '')
-
-            # Téléchargement de l'image et sauvegarde localement
-            img_data = requests.get(img_url).content
-            img_name = article.img['src'].split("/")[-1]
-            img_path = f"data/images/{img_name}"
-            with open(img_path, 'wb') as handler:
-                handler.write(img_data)
-
-            # Ajout des informations du livre à la liste
-            all_books.append({'Titre': all_book_title, 'Prix': all_book_price, 'Disponibilité': all_book_availability, 'Catégorie': all_book_category_link.text.strip()})
-
-        # Récupération de l'URL de la page suivante
-        next_page = all_book_category_soup.find('li', {'class': 'next'})
+for link in all_book_category_links:
+    category_url = url + link.get('href')
+    while category_url:
+        category_page = requests.get(category_url)
+        category_soup = BeautifulSoup(category_page.content, 'html.parser')
+        data = category_soup.find_all('article', {'class': 'product_pod'})
+        all_books += transform(data, link.text.strip()).to_dict('records')
+        next_page = category_soup.find('li', {'class': 'next'})
         if next_page:
-            all_book_category_url = url + next_page.a.get('href')
+            category_url = url + next_page.a.get('href')
         else:
-            all_book_category_url = None
-
-# Créer un DataFrame pandas avec les informations extraites
-book_data = pd.DataFrame(all_books).to_csv('data/all_books_all_category.csv', sep=',', index=False)
-
+            category_url = None
+load(pd.DataFrame(all_books), 'data/all_books_all_category.csv')
